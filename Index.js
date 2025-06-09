a653f68a080da8f9212fd6928c60bcca8703d1f4
Md Nazmul Hossen <nazmulhossen.hstu.21@gmail.com>
2025-06-09 13:42:07 +0600

    installed firebase admin and securing firebase json file to base64

--- Index.js
+++ Index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
+import admin from 'firebase-admin';
 
 const app = express();
 const port = process.env.PORT || 3000;
